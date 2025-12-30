9524c2f71f447eb028813723fb0c67e78166bc59
Romain Marziali <romainmarziali@gmail.com>
2025-12-30 18:58:00 +0100

    New commit

--- backend/angular/server/angular-app-manifest.mjs
+++ backend/angular/server/angular-app-manifest.mjs
@@ -12,9 +12,9 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 24747, hash: 'e0bf4c6eff768abc697d313b1f21c59d21ef060db2233518535080251aaa81b2', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 17055, hash: '9224d8148b554fd6f7809f3883e364f137aef74f71bf3cfd39d8b9b8d01d8c81', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 91472, hash: 'ee9eead9dbf6c9b41e1c1e750c62e401e9752b146d3fecc3cbd51afd6b866ce1', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 24747, hash: '167912210c8a9616006f1e3f2c1cdfc5f013a2e088848db38fc73c5334878677', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 17055, hash: 'bc8f0ad79f1887a49f447e47878d882257c6a065053862304860de318aa9b423', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 91472, hash: '16cc77655bb2590fdf4a358730e206f0371ecfe9b7cb777320e17b72daa76090', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
     'styles-SRBIV2EV.css': {size: 8248, hash: 'DvQ8mWIl6w8', text: () => import('./assets-chunks/styles-SRBIV2EV_css.mjs').then(m => m.default)}
   },
 };
