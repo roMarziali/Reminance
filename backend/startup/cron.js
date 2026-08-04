8326d593ded3b41512d032a44ca3f3bc9da05efc
Romain Marziali <romainmarziali@gmail.com>
2026-08-04 19:37:15 +0200

    change in cron

--- backend/startup/cron.js
+++ backend/startup/cron.js
@@ -2,5 +2,4 @@ const CronJob = require('cron').CronJob;
 
 const backup = require('../models/backup').backup;
 
-new CronJob('0 3 1 * *', backup).start();
-
+new CronJob('0 0 7 1 * *', backup).start();
